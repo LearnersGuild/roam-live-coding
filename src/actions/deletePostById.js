97f64fb89d8133c264ec5d2fe56725367e658d08
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-03-06 11:10:07 -0800

    Create deletePostById stub; start writing tests

---
+++ src/actions/deletePostById.js
@@ -0,0 +1,10 @@
+const db = require('../db/db')
+
+/**
+ * Delete a post with the given id
+ * @param {number} id 
+ * @returns {promise} - Promise whose resolution is unimportant
+ */
+const deletePostById = (id) => {
+  return Promise.resolve({})
+}
--- test/actions/post_actions.js
+++ test/actions/post_actions.js
@@ -22,7 +22,7 @@ describe('add new post', function() {
     'body': 'I want to move here! live here! be here all the time!',
   }
 
-  beforeEach(() => {
+  beforeEach(('reset the db and add a post') => {
     return resetDb()
     .then(() => addPost(postData.city_id, postData.user_id, postData.title, postData.body))
     .then(post => { newPost = post })
@@ -59,7 +59,6 @@ describe('getPostById', function() {
         })
   })
   it('return null when the post does not exist', () => {
-    before('reset the db and run getpostData')
     return getPostById(89720348590)
       .then(postRow => {
         expect(postRow).to.be.null
@@ -68,34 +67,14 @@ describe('getPostById', function() {
   })
 })
 
-// describe('getpostData', function() {
-//   let postData
-//   const extraDataProps = ['posts']
-//   const fullpostProps = post_PROPS.concat(extraDataProps)
-//   context('post exists', () => {
-//     before('reset the database and retrieve existing post', () => {
-//       return resetDb()
-//         .then(() => getpostData('test@test.test'))
-//         .then(result => postData = result)
-//     })
-//     fullpostProps.forEach(prop => {
-//       it(`returns an object with the '${prop}' property`, () => {
-//         expect(postData).to.have.property(prop)
-//       })
-//     })
-//     extraDataProps.forEach(prop => {
-//       it(`returns an array value for the ${prop} property`, () => {
-//         expect(postData[prop]).to.be.an('array')
-//       })
-//       it(`returns an array of length 1 the ${prop} property`, () => {
-//         expect(postData[prop]).to.have.length(1)
-//       })
-//     })
-//   })
-//   it('return null when the post does not exist', () => {
-//     return getpostData('doesntexist@nowhere.com')
-//       .then(postData => {
-//         expect(postData).to.be.null
-//       })
-//   })
-// })
+describe('deletePostById', function() {
+  let postCountBefore
+  beforeEach('reset and seed the db', () => {
+    resetDb()
+      .then(() => {
+        db.one(`SELECT COUNT(id) FROM posts`)
+      })
+      .then(count => postCountBefore = count)
+  })
+  it('deletes the post from the database')
+})
