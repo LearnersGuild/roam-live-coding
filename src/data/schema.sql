c2520c4331573a553b618d0d3fd248d54cd30689
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-03-06 11:08:37 -0800

    Start to create file structure for actions TDD

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name NOT NULL TEXT,
-  email NOT NULL TEXT UNIQUE,
-  password NOT NULL TEXT,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
   joined_at DATE DEFAULT CURRENT_DATE,
   user_image_url TEXT,
-  primary_city NOT NULL TEXT
+  primary_city TEXT NOT NULL
 );
 CREATE TABLE cities (
   id SERIAL PRIMARY KEY,
@@ -19,8 +19,8 @@ CREATE TABLE posts ( -- users_cities
   id SERIAL PRIMARY KEY,
   city_id INTEGER NOT NULL REFERENCES cities,
   user_id INTEGER NOT NULL REFERENCES users,
-  title VARCHAR(200) NOT NULL CHECK (x != ''),
-  body TEXT NOT NULL CHECK (x != ''),
+  title VARCHAR(200) NOT NULL CHECK (title != ''),
+  body TEXT NOT NULL CHECK (body != ''),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE comments (
