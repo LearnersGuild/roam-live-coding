909679ce2c435de42b9f568b5aa728424bd66602
James McCormack <thejamesmccormack@gmail.com>
2018-03-27 16:01:47 -0700

    write route and tests for posts patch. not fully working

--- src/actions/updatePostById.js
+++ src/actions/updatePostById.js
@@ -2,14 +2,14 @@ const db = require('../db/db')
 
 /**
  * Update a post in the database
- * 
+ *
  * @param {number} id - ID of post to update
  * @param {object} newData - keys: 'body' and/or 'title'
- * @returns {promise} - Promise that resolves to an object 
+ * @returns {promise} - Promise that resolves to an object
  * representing the updated row in the posts table.
  *   -> Resolve to null if id doesn't exist in db.
  */
-const updatePostById = function(id, newData) {
+const updatePostById = function(id, userId, newData) {
   let query = 'UPDATE posts SET\n'
   const updates = []
   for (let column in newData) {
@@ -18,6 +18,7 @@ const updatePostById = function(id, newData) {
   query += updates.join(', ')
   query += `
     WHERE id = $/id/
+    AND user_id = $/userId/
     RETURNING *
   `
   return db.oneOrNone(query, Object.assign(newData, {id}))
